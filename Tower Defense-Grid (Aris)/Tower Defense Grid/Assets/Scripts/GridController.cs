371a9332753a2339683f4b7b952b918ba082e9d1
Padiaten <dai16002@uom.edu.gr>
2017-04-13 00:12:33 +0300

    TowerDefense
    Composed by Aris Christoforidis

---
+++ Tower Defense-Grid (Aris)/Tower Defense Grid/Assets/Scripts/GridController.cs
@@ -0,0 +1,139 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridController : MonoBehaviour {
+	
+	List<List<string>> map = new List<List<string>>();
+	//Έχει τα πλακίδια-αφετηρία
+	protected List<GameObject> StartTiles = new List<GameObject>();
+	void Start () {
+
+        //string[] map = System.IO.File.ReadAllLines("C:\\Users\\Aris\\Documents\\Tower Defense Grid\\Assets\\");
+
+
+
+        /*map.Add(new List<string>(){"X","X","X","X","SD","X","X","X","X","X"});
+		map.Add(new List<string>(){"X","X","X","X","D","X","D","L","L","X"});
+		map.Add(new List<string>(){"X","X","X","X","D","X","D","X","U","X"});
+		map.Add(new List<string>(){"X","X","D","L","LR","R","DR","R","U","X"});
+		map.Add(new List<string>(){"X","X","D","X","X","X","D","X","X","X"});
+		map.Add(new List<string>(){"SR","R","D","X","X","X","D","X","X","X"});
+		map.Add(new List<string>(){"X","X","D","X","X","X","D","X","X","X"});
+		map.Add(new List<string>(){"X","X","R","R","D","L","L","X","X","X"});
+		map.Add(new List<string>(){"X","X","X","X","D","X","X","X","X","X"});
+		map.Add(new List<string>(){"X","X","X","X","E","X","X","X","X","X"});*/
+
+        map.Add(new List<string>(){"X","X","X","SD","X","X","X","X","X"});
+		map.Add(new List<string>(){"X","X","X","R","D","X","X","X","X"});
+		map.Add(new List<string>(){"X","X","X","X","D","X","X","X","X"});
+		map.Add(new List<string>(){"X","X","X","X","D","X","X","X","X"});
+		map.Add(new List<string>(){"X","X","D","L","LD","L","L","X","X"});
+		map.Add(new List<string>(){"X","X","D","X","D","X","U","X","X"});
+		map.Add(new List<string>(){"X","X","D","X","R","R","U","X","X"});
+		map.Add(new List<string>(){"X","X","R","D","X","X","X","X","X"});
+		map.Add(new List<string>(){"X","X","X","E","X","X","X","X","X"});
+
+
+
+        drawMap();
+
+	
+			
+
+	}
+	
+
+
+	void drawMap(){
+		//Scan 1 of 2,identify general tile type and place on grid
+		for(int i=0;i<map.Count;i++){
+			for(int j=0;j<map[i].Count;j++){
+				GameObject go = new GameObject();
+				go.AddComponent<SpriteRenderer>();
+				//go.AddComponent<SpriteRenderer>();
+				if(map[i][j] == "X" || map[i][j] == "B" ){
+					go.AddComponent<GrassTile>();
+					go.GetComponent<GrassTile>().initializeTile(j,map[j].Count-1-i);
+					go.name = "G" + " " + j + "," + (map[j].Count-1-i).ToString();
+					if(map[i][j] == "B"){
+						go.GetComponent<GrassTile>().set_canPlaceTower(false);
+					}
+				}else{
+					go.AddComponent<PathTile>();
+					go.GetComponent<PathTile>().initializeTile(j,map[j].Count-1-i);
+					go.name = "P" + " " + j + "," + (map[j].Count-1-i).ToString();
+
+					//Ελένχει αν ειναι αφετηρια και το βαζει στη λιστα
+					}if(map[i][j].Contains("S")){
+						StartTiles.Add(go);
+					}
+			}
+		}
+
+		//Scan 2 of 3,assign previous and next tiles to each tile
+		for(int i=0;i<map.Count;i++){
+			for(int j=0;j<map[i].Count;j++){
+				print("Tried to access:" + map[map.Count-1-i][j]+" " + j + "," + i);
+				if(map[map.Count-1-i][j] != "X" && map[map.Count-1-i][j] != "B"){
+					string cur_tile_name ="P" + " " + j + "," + i;
+					print("Current Tile:" + cur_tile_name);
+					GameObject cur_tile = GameObject.Find(cur_tile_name);
+
+					if(map[map.Count-1-i][j].Contains("U")){
+						string next_tile_name = "P" + " " + j + "," + (i+1).ToString();
+						GameObject p = GameObject.Find(next_tile_name);
+						print("Next Tile:" + next_tile_name);
+						p.GetComponent<PathTile>().add_PrevTile(cur_tile);
+						cur_tile.GetComponent<PathTile>().add_NextTile(p);
+					}
+
+					if(map[map.Count-1-i][j].Contains("D")){
+						string next_tile_name = "P" + " " + j + "," + (i-1).ToString();
+						GameObject p = GameObject.Find(next_tile_name);
+						print("Next Tile:" + p);
+						p.GetComponent<PathTile>().add_PrevTile(cur_tile);
+						cur_tile.GetComponent<PathTile>().add_NextTile(p);
+					}
+
+					if(map[map.Count-1-i][j].Contains("L")){
+						string next_tile_name = "P" + " " + (j-1).ToString() + "," + i;
+						GameObject p = GameObject.Find(next_tile_name);
+						print("Next Tile:" + next_tile_name);
+						p.GetComponent<PathTile>().add_PrevTile(cur_tile);
+						cur_tile.GetComponent<PathTile>().add_NextTile(p);
+					}
+
+					if(map[map.Count-1-i][j].Contains("R")){
+						string next_tile_name = "P" + " " + (j+1).ToString() + "," + i;
+						GameObject p = GameObject.Find(next_tile_name);
+						print("Next Tile:" + next_tile_name);
+						p.GetComponent<PathTile>().add_PrevTile(cur_tile);
+						cur_tile.GetComponent<PathTile>().add_NextTile(p);
+					}
+				}
+			}
+		}
+
+		//Scan 3 of 3,identify specific path tile and orientation
+		for(int i=0;i<map.Count;i++){
+			for(int j=0;j<map[i].Count;j++){
+				if(map[map.Count-1-i][j] != "X" && map[map.Count-1-i][j] != "B"){
+					string tile_name = "P" + " " + j + "," + i;
+					int mapsize_x = map.Count;
+					int mapsize_y = map[j].Count;
+					GameObject go = GameObject.Find(tile_name);
+					go.GetComponent<PathTile>().set_PathTile_Type(mapsize_x,mapsize_y);
+				}
+			
+			}
+		}
+
+	}
+
+	public List<GameObject> GetStartTiles(){
+		return StartTiles;
+	}
+
+
+}
---
+++ Tower Defense-Grid (Aris)/Tower Defense Grid/Assets/Scripts/Movement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+	private float speed;
+	private GameObject g;
+	private GameObject g1;
+	private Vector2 vectorEnemy;
+	private Vector2 vectorNext;
+	private Vector2 v2;
+
+    private bool flag;
+
+	// Use this for initialization
+	void Start () {
+		vectorEnemy = transform.position;
+		speed = 1f;
+        List<GameObject> go = Camera.main.GetComponent<GridController>().GetStartTiles();
+        g = go[0];
+		g = g.GetComponent<PathTile> ().getNextTile_Random ();
+		vectorNext = g.GetComponent<Tile> ().getCoords ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+            //Vector2 dir = new Vector2(vectorNext.x-transform.position.x,vectorNext.y-transform.position.y);
+            //transform.Translate(3*Time.deltaTime*dir.normalized,Space.World);
+            transform.position = Vector2.MoveTowards(transform.position, vectorNext,3*Time.deltaTime);
+        if (Vector2.Distance((Vector2)transform.position, vectorNext) < 0.2 && !flag) {
+            g = g.GetComponent<PathTile>().getNextTile_Random();
+            vectorNext = g.GetComponent<Tile>().getCoords();
+            if (g.GetComponent<PathTile>().NextTiles.Count == 0)
+            {
+                print("TRUE");
+                flag = true;
+            }
+        }
+        if (Mathf.Approximately(transform.position.x, vectorNext.x) && Mathf.Approximately(transform.position.y, vectorNext.y) && flag)
+            Destroy(this.gameObject);
+	}
+}
